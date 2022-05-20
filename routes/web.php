89106da1b7ace3b34d939b64c168abb3f9c41820
daffa <daffaahmad06@yahoo.com>
2022-05-20 11:28:38 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+Route::get('/contact-form', [ContactController::class, 'contactForm'])->name('contact-form');
+
+Route::post('/contact-form', [ContactController::class, 'storeContactForm'])->name('contact-form.store');
