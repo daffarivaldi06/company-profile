6d48f7019dc60dbed23c804e3d17ce0bea1f1557
nans1899 <anandabakti18@gmail.com>
2022-05-27 13:57:40 +0700

    icon-box

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -243,7 +243,7 @@
 
         <div class="row">
           <div class="col-lg-4 col-md-6 d-fixed align-items-fixed" data-aos="zoom-in" data-aos-delay="100">
-            <div class="icon-box">
+            <div class="icon-box" style="height: 450px">
               <div class="icon"><i class="bx bxl-dribbble"></i></div>
               <h4><a href="">Lorem Ipsum</a></h4>
               <p>- BI Reporting & Data viz.<br>
@@ -256,7 +256,7 @@
           </div>
 
           <div class="col-lg-4 col-md-6 d-fixed align-items-fixed mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
-            <div class="icon-box">
+            <div class="icon-box" style="height: 450px">
               <div class="icon"><i class="bx bx-file"></i></div>
               <h4><a href="">Sed ut perspiciatis</a></h4>
               <p>- All data necessary for business<br>
@@ -268,7 +268,7 @@
           </div>
 
           <div class="col-lg-4 col-md-6 d-fixed align-items-fixed mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="300">
-            <div class="icon-box">
+            <div class="icon-box" style="height: 450px">
               <div class="icon"><i class="bx bx-tachometer"></i></div>
               <h4><a href="">Magni Dolores</a></h4>
               <p>- Data cleansing, data dedup<br>
@@ -282,7 +282,7 @@
           </div>
 
           <div class="col-lg-4 col-md-6 d-fixed align-items-fixed mt-4" data-aos="zoom-in" data-aos-delay="100">
-            <div class="icon-box">
+            <div class="icon-box" style="height: 450px">
               <div class="icon"><i class="bx bx-world"></i></div>
               <h4><a href="">Nemo Enim</a></h4>
               <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
@@ -290,7 +290,7 @@
           </div>
 
           <div class="col-lg-4 col-md-6 d-fixed align-items-fixed mt-4" data-aos="zoom-in" data-aos-delay="200">
-            <div class="icon-box">
+            <div class="icon-box" style="height: 450px">
               <div class="icon"><i class="bx bx-slideshow"></i></div>
               <h4><a href="">Dele cardo</a></h4>
               <p>Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur</p>
@@ -298,7 +298,7 @@
           </div>
 
           <div class="col-lg-4 col-md-6 d-fixed align-items-fixed mt-4" data-aos="zoom-in" data-aos-delay="300">
-            <div class="icon-box">
+            <div class="icon-box" style="height: 450px">
               <div class="icon"><i class="bx bx-arch"></i></div>
               <h4><a href="">Divera don</a></h4>
               <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
